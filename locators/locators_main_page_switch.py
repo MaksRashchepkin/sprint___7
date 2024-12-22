d9a3a060568ac9c0fa819403977d9c18eba39c81
Maks Rashchepkin <m.rashepkin2018@gmail.com>
2024-12-22 15:56:21 +0300

    sprint___7

--- locators/locators_main_page_switch.py
+++
@@ -1,5 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class MainPageSwitchLocators:
-    logo_header_scooter = (By.XPATH, ".//a[@href='/']")
-    logo_header_yandex = (By.XPATH, ".//a[@href='//yandex.ru']")
